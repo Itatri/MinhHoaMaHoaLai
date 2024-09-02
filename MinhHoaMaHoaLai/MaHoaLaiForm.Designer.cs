731aca265c4714f8ffd8b8ad79df534445557987
Itatri <trihoangvan16092003@gmail.com>
2024-09-02 21:16:02 +0700

    Add project files.

---
+++ MinhHoaMaHoaLai/MaHoaLaiForm.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace MinhHoaMaHoaLai
+{
+    partial class MaHoaLaiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQ = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtE = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSymmetricKey = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPlainText = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtEncryptedText = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtDecryptedData = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtEncryptedKey = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtP
+            // 
+            this.txtP.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtP.Location = new System.Drawing.Point(64, 74);
+            this.txtP.Name = "txtP";
+            this.txtP.Size = new System.Drawing.Size(100, 22);
+            this.txtP.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(37, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "p";
+            // 
+            // txtQ
+            // 
+            this.txtQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtQ.Location = new System.Drawing.Point(235, 74);
+            this.txtQ.Name = "txtQ";
+            this.txtQ.Size = new System.Drawing.Size(100, 22);
+            this.txtQ.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(208, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "q";
+            // 
+            // txtE
+            // 
+            this.txtE.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtE.Location = new System.Drawing.Point(398, 74);
+            this.txtE.Name = "txtE";
+            this.txtE.Size = new System.Drawing.Size(100, 22);
+            this.txtE.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(371, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "e";
+            // 
+            // txtSymmetricKey
+            // 
+            this.txtSymmetricKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtSymmetricKey.Location = new System.Drawing.Point(64, 162);
+            this.txtSymmetricKey.Name = "txtSymmetricKey";
+            this.txtSymmetricKey.Size = new System.Drawing.Size(100, 22);
+            this.txtSymmetricKey.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(37, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "K";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(36, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(271, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Nhập giá trị cho thuật toán RSA";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label6.Location = new System.Drawing.Point(37, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(341, 24);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Nhập khóa đối xứng cho Caesar cipher";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label7.Location = new System.Drawing.Point(36, 210);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(227, 24);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Nhập dữ liệu cần mã hóa ";
+            // 
+            // txtPlainText
+            // 
+            this.txtPlainText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtPlainText.Location = new System.Drawing.Point(41, 250);
+            this.txtPlainText.Multiline = true;
+            this.txtPlainText.Name = "txtPlainText";
+            this.txtPlainText.Size = new System.Drawing.Size(457, 32);
+            this.txtPlainText.TabIndex = 15;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(235, 311);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 16;
+            this.btnEncrypt.Text = "Mã hóa ";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(232, 548);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 17;
+            this.btnDecrypt.Text = "Giải mã";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtEncryptedText
+            // 
+            this.txtEncryptedText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtEncryptedText.Location = new System.Drawing.Point(40, 401);
+            this.txtEncryptedText.Multiline = true;
+            this.txtEncryptedText.Name = "txtEncryptedText";
+            this.txtEncryptedText.Size = new System.Drawing.Size(457, 32);
+            this.txtEncryptedText.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label8.Location = new System.Drawing.Point(35, 361);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(142, 24);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Dữ liệu mã hóa ";
+            // 
+            // txtDecryptedData
+            // 
+            this.txtDecryptedData.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtDecryptedData.Location = new System.Drawing.Point(39, 627);
+            this.txtDecryptedData.Multiline = true;
+            this.txtDecryptedData.Name = "txtDecryptedData";
+            this.txtDecryptedData.Size = new System.Drawing.Size(457, 32);
+            this.txtDecryptedData.TabIndex = 21;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label9.Location = new System.Drawing.Point(34, 587);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(203, 24);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Dữ liệu sau khi giải mã ";
+            // 
+            // txtEncryptedKey
+            // 
+            this.txtEncryptedKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtEncryptedKey.Location = new System.Drawing.Point(41, 493);
+            this.txtEncryptedKey.Multiline = true;
+            this.txtEncryptedKey.Name = "txtEncryptedKey";
+            this.txtEncryptedKey.Size = new System.Drawing.Size(457, 32);
+            this.txtEncryptedKey.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label10.Location = new System.Drawing.Point(36, 453);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(202, 24);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Khóa đối xứng mã hóa";
+            // 
+            // MaHoaLaiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 682);
+            this.Controls.Add(this.txtEncryptedKey);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtDecryptedData);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtEncryptedText);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtPlainText);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSymmetricKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtE);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtQ);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtP);
+            this.Controls.Add(this.label2);
+            this.Name = "MaHoaLaiForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtE;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSymmetricKey;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPlainText;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtEncryptedText;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtDecryptedData;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtEncryptedKey;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ MinhHoaMaHoaLai/MaHoaLaiForm.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinhHoaMaHoaLai
+{
+    public partial class MaHoaLaiForm : Form
+    {
+        public MaHoaLaiForm()
+        {
+            InitializeComponent();
+        }
+        public class RSA
+        {
+            public int p { get; set; }
+            public int q { get; set; }
+            public int n { get; private set; }
+            public int z { get; private set; }
+            public int e { get; set; }
+            public int d { get; private set; }
+
+            public RSA(int p, int q)
+            {
+                this.p = p;
+                this.q = q;
+                this.n = p * q;
+                this.z = (p - 1) * (q - 1);
+                this.e = FindE();
+                this.d = FindD();
+            }
+
+            // Tìm số e thỏa mãn gcd(e, z) = 1 và 1 < e < z
+            private int FindE()
+            {
+                for (int i = 2; i < z; i++)
+                {
+                    if (GCD(i, z) == 1)
+                        return i;
+                }
+                return -1; // Không tìm thấy e
+            }
+
+            // Tìm số d thỏa mãn (e * d) mod z = 1
+            private int FindD()
+            {
+                int d = 1;
+                while ((e * d) % z != 1)
+                {
+                    d++;
+                }
+                return d;
+            }
+
+            // Tìm ước số chung lớn nhất (GCD)
+            private int GCD(int a, int b)
+            {
+                while (b != 0)
+                {
+                    int temp = b;
+                    b = a % b;
+                    a = temp;
+                }
+                return a;
+            }
+
+            // Mã hóa dữ liệu
+            public int Encrypt(int m)
+            {
+                return ModExp(m, e, n);
+            }
+
+            // Giải mã dữ liệu
+            public int Decrypt(int c)
+            {
+                return ModExp(c, d, n);
+            }
+
+            // Tính toán m^e mod n 
+            private int ModExp(int baseVal, int exp, int mod)
+            {
+                int result = 1;
+                while (exp > 0)
+                {
+                    if ((exp % 2) == 1)
+                    {
+                        result = (result * baseVal) % mod;
+                    }
+                    baseVal = (baseVal * baseVal) % mod;
+                    exp /= 2;
+                }
+                return result;
+            }
+        }
+        public class CaesarCipher
+        {
+            public int Key { get; set; }
+
+            public CaesarCipher(int key)
+            {
+                Key = key;
+            }
+
+            public string Encrypt(string plainText)
+            {
+                StringBuilder encryptedText = new StringBuilder();
+                foreach (char ch in plainText)
+                {
+                    if (char.IsLetter(ch))
+                    {
+                        char d = char.IsUpper(ch) ? 'A' : 'a';
+                        int newIndex = (((ch - d) + Key) % 26); 
+                        encryptedText.Append((char)(newIndex + d));
+                    }
+                    else
+                    {
+                        encryptedText.Append(ch);
+                    }
+                }
+                return encryptedText.ToString();
+            }
+
+            public string Decrypt(string cipherText)
+            {
+                StringBuilder decryptedText = new StringBuilder();
+                foreach (char ch in cipherText)
+                {
+                    if (char.IsLetter(ch))
+                    {
+                        char d = char.IsUpper(ch) ? 'A' : 'a';
+                        int newIndex = (((ch - d) - Key + 26) % 26); 
+                        decryptedText.Append((char)(newIndex + d));
+                    }
+                    else
+                    {
+                        decryptedText.Append(ch);
+                    }
+                }
+                return decryptedText.ToString();
+            }
+        }
+
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+
+            int p = int.Parse(txtP.Text);
+            int q = int.Parse(txtQ.Text);
+            int eValue = int.Parse(txtE.Text);
+            int symmetricKey = int.Parse(txtSymmetricKey.Text);
+            string plainText = txtPlainText.Text;
+
+            // Khởi tạo thuật toán RSA
+            RSA rsa = new RSA(p, q);
+            rsa.e = eValue;
+
+            // Mã hóa dữ liệu gốc bằng Caesar cipher
+            CaesarCipher caesar = new CaesarCipher(symmetricKey);
+            string encryptedText = caesar.Encrypt(plainText);
+
+            // Mã hóa khóa đối xứng bằng RSA
+            int encryptedSymmetricKey = rsa.Encrypt(symmetricKey);
+
+            // Hiển thị dữ liệu mã hóa
+            txtEncryptedText.Text = encryptedText; // Hiển thị dữ liệu mã hóa
+            txtEncryptedKey.Text = encryptedSymmetricKey.ToString(); // Hiển thị khóa đối xứng mã hóa
+
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            int p = int.Parse(txtP.Text);
+            int q = int.Parse(txtQ.Text);
+            int eValue = int.Parse(txtE.Text);
+            int encryptedSymmetricKey = int.Parse(txtEncryptedKey.Text); // Lấy khóa đối xứng mã hóa
+            string encryptedText = txtEncryptedText.Text; // Lấy dữ liệu mã hóa
+
+            // Khởi tạo thuật toán RSA
+            RSA rsa = new RSA(p, q);
+            rsa.e = eValue;
+
+            // Giải mã khóa đối xứng từ RSA
+            int decryptedSymmetricKey = rsa.Decrypt(encryptedSymmetricKey);
+
+            // Sử dụng khóa đối xứng để giải mã dữ liệu
+            CaesarCipher caesar = new CaesarCipher(decryptedSymmetricKey);
+            string decryptedText = caesar.Decrypt(encryptedText);
+
+            // Hiển thị dữ liệu đã giải mã
+            txtDecryptedData.Text = decryptedText;
+        }
+    }
+}
